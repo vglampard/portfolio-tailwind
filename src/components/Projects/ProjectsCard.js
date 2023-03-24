0ddb13a5d3aa497d2de9fe66f4cf71ff241bb5d5
Victoria Lampard <vglampard@protonmail.com>
2023-03-24 16:47:38 +0000

    undid MUI issue

--- src/components/Projects/ProjectsCard.js
+++ src/components/Projects/ProjectsCard.js
@@ -9,7 +9,7 @@ export default function ProjectsCard({ project }) {
     <motion.div
     variants={fadeIn("down", "tween", 0.2, 1)}
     >
-      <div className="bg-yellow-50 m-5 p-4 rounded-lg drop-shadow-lg">
+      <div className="bg-slate-50 m-5 p-4 rounded drop-shadow-lg">
         <img src={image} alt="project" />
         <h1>{project.description}</h1>
       </div>
