9b25aba7ccc6cad8482cdac28abb35366ab546bb
Victoria Lampard <vglampard@protonmail.com>
2023-03-24 19:29:51 +0000

    playing around with carousels

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import headshot from "./pp.png"
+import React from "react";
+import ImageCarousel from "../Carousel/Carousel";
+import headshot from "./pp.png";
 export default function Contact() {
   return (
     <div className="flex items-center justify-center">
       <div className="w-3/4 flex flex-col justify-center items-center">
         <h1> contact me</h1>
-        <p className="text-sm">At vero eos et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat</p>
-<img src={headshot} alt="headshot of victoria lampard"/>
+        <ImageCarousel />
+        <p className="text-sm">
+          At vero eos et voluptates repudiandae sint et molestiae non
+          recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut
+          reiciendis voluptatibus maiores alias consequatur aut perferendis
+          doloribus asperiores repellat
+        </p>
+        <img src={headshot} alt="headshot of victoria lampard" />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
