076032c1aaecdc29ba12cf86ad62d15e5e608252
Victoria Lampard <vglampard@protonmail.com>
2023-03-26 16:59:48 +0100

    images added

--- src/components/Projects/ProjectsCard.js
+++ src/components/Projects/ProjectsCard.js
@@ -1,10 +1,16 @@
 import React from "react";
-import image from "../../images/password.png";
-import { fadeIn } from "../../utils/motion";
 import { motion } from "framer-motion";
-
+import password from "./projectImages/password.png"
+import amigo from "./projectImages/amigo.png"
+import nostrami from "./projectImages/nostrami.png"
+import payback from "./projectImages/payback.png"
+import weather from "./projectImages/weather.png"
 export default function ProjectsCard({ project, MODAL_STATES }) {
-  console.log("proj", project);
+
+  const IMAGES = {
+    password, amigo, nostrami, payback, weather
+  }
+
   return (
     <div>
       <motion.div
@@ -15,9 +21,9 @@ export default function ProjectsCard({ project, MODAL_STATES }) {
         <div className="bg-slate-50  m-2 rounded drop-shadow-lg">
           <img 
           // src={`./projectImages/${project.image}`}
-          src="./projectImages/password.png" alt="project" />
+          src={IMAGES[project.image]} alt="project" />
           <div className="flex justify-between">
-            <h1>{project.title}</h1>
+            <h1>{}</h1>
             <button
               className="bg-slate-300 text-slate-900 m-3 active:bg-slate-50 uppercase text-sm rounded shadow hover:shadow-xl w-10 outline-none justify-center focus:outline-none flex items-center ease-linear transition-all duration-150"
               type="button"
--- src/constants/projectsData.js
+++ src/constants/projectsData.js
@@ -1,34 +1,43 @@
 export const PROJECTS = [
+
   {
-    title: "Amigo - Your travel companion",
+    title: "Amigo - personal travel companion",
+    subtitle: "TECH STACK INFO",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "amigo",
+      link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Nostrami - Nostr client (in development)",
+    subtitle: "TECH STACK INFO",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "nostrami",
+      link: "https://reactbootcamp.com",
+  },
+  {
+    title: "The PayBack Machine - advanced bill settlements",
+    subtitle: "TECH STACK INFO",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "payback",
+      link: "https://reactbootcamp.com",
+  },
+  {
+    title: "BetterPassword - SHA-256 password retrieval",
     subtitle: "TECHSTACK INFO",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "password.png",
+    image: "password",
     link: "https://reactbootcamp.com",
   },
-  // {
-  //   title: "Dingo' Directives - your bootcamper learning hub",
-  //   subtitle: "TECH STACK INFO",
-  //   description:
-  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-  //     image: "./projectImages/password.png",
-  //     link: "https://reactbootcamp.com",
-  // },
-  // {
-  //   title: "GoGram - your visual search engine",
-  //   subtitle: "TECH STACK INFO",
-  //   description:
-  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-  //     image: "./projectImages/password.png",
-  //     link: "https://reactbootcamp.com",
-  // },
-  // {
-  //   title: "The PayBack Machine - never owe or be owed",
-  //   subtitle: "TECH STACK INFO",
-  //   description:
-  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-  //     image: "./projectImages/password.png",
-  //     link: "https://reactbootcamp.com",
-  // },
+  {
+    title: "WeatherVisuals - weather display",
+    subtitle: "TECH STACK INFO",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "weather",
+      link: "https://reactbootcamp.com",
+  },
 ];
