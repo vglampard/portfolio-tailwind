e6267a615b6cb8813d1600dfb852a6c8dc5a042b
Victoria Lampard <vglampard@protonmail.com>
2023-03-31 17:29:23 +0100

    icons in repo

--- src/components/Skills/SkillCard.js
+++ src/components/Skills/SkillCard.js
@@ -1,5 +1,18 @@
 import React, { useState } from "react";
 import sql from "../../icons/sql-server.png";
+import jest from "../../icons/jest.webp";
+import javascript from "../../icons/javascript.png";
+import html from "../../icons/html.png";
+import css from "../../icons/css-3.png";
+import react from "../../icons/atoms.png";
+import typescript from "../../icons/typescript.png";
+import agile from "../../icons/agile.png";
+import api from "../../icons/api.png";
+import java from "../../icons/java.png";
+import node from "../../icons/node.png";
+import pair from "../../icons/pair.png";
+import lead from "../../icons/lead.png";
+import redux from "../../icons/redux.png";
 
 export default function SkillCard(area) {
   const [seeMore, setSeeMore] = useState(false);
@@ -7,6 +20,16 @@ export default function SkillCard(area) {
   function handleSeeMore() {
     setSeeMore(!seeMore);
   }
+
+  const ICON_REF = {
+    frontEnd: { javascript, html, css, react, typescript },
+    backEnd: { sql, api, node },
+    toolsAndTest: { jest },
+    principles: { agile, pair, lead },
+    learning: { java, javascript, redux },
+  };
+
+  console.log("SKILL:", area.skill.ref, ICON_REF[area.skill.ref][javascript]);
   return (
     <div className="bg-slate-200 flex-col w-[90%] p-2 rounded drop-shadow-lg flex text-justify">
       <div className="flex">
@@ -18,9 +41,17 @@ export default function SkillCard(area) {
           {seeMore ? " <<<" : " >>>"}
         </h1>
         <div className="flex flex-row w-full align-center justify-end">
-          <img src={sql} alt="sql" className="h-7" />
-          <img src={sql} alt="sql" className="h-7" />
-          <img src={sql} alt="sql" className="h-7" />
+          {area.skill.icons.map((icon) => {
+            return (
+                // <>
+                // <p>{area.skill.ref} </p>
+                // <p>{ICON_REF[area.skill.ref][icon]}</p>
+                // </>
+              <img src={ICON_REF[area.skill.ref][icon]} alt="#" className="h-7" />
+            );
+          })}
+
+          {/* <img src={ICON_REF.frontEnd[icon]} alt="sql" className="h-7" /> */}
         </div>
       </div>
       {seeMore && <p className="px-2 text-sm">{area.skill.subskills}</p>}
--- src/constants/skillsData.js
+++ src/constants/skillsData.js
@@ -1,13 +1,38 @@
 export const SKILLS = [
-    {area: "Front End", 
-subskills:" JavaScript, HTML, CSS, TypeScript, React, Next.js, Tailwind, MUI, Figma, Redux, UI /UX"},
-{area: "Back End", 
-subskills:"Node.js, PostgreSQL, relational databases, Express, RESTful APIs"},
-{area: "Tools & Test", 
-subskills:"Postman, Cypress, Jest, Supertest, React testing library, Render/Netlify, version control (Git), Jira, AWS Cloud Practitioner Essentials, Firebase"},
-{area: "Principles", 
-subskills:"Agile working, test-driven development, pair programming, computational thinking, team leadership, sprint planning, computational thinking, code review, debugging, remote working, Trello"},
-{area: "Learning", 
-subskills:"Redux, AWS, Nostr, more about data structures and algorithms with a focus on scripting, and dipping my toes into Java..."},
-]
+  {
+    area: "Front End",
+    ref: "frontEnd",
+    subskills:
+      " JavaScript, HTML, CSS, TypeScript, React, Next.js, Tailwind, MUI, Figma, Redux, UI /UX",
+    icons: ["sql", "javascript", "html", "react"],
+  },
 
+  {
+    area: "Back End",
+    ref:"backEnd",
+    subskills:
+      "Node.js, PostgreSQL, relational databases, Express, RESTful APIs",
+      icons: ["sql", "javascript", "html", "react"],
+  },
+  {
+    area: "Tools & Test",
+    ref:"toolsAndTest",
+    subskills:
+      "Postman, Cypress, Jest, Supertest, React testing library, Render/Netlify, version control (Git), Jira, AWS Cloud Practitioner Essentials, Firebase",
+      icons: ["sql", "javascript", "html", "react"],
+  },
+  {
+    area: "Principles",
+    ref:"principles",
+    subskills:
+      "Agile working, test-driven development, pair programming, computational thinking, team leadership, sprint planning, computational thinking, code review, debugging, remote working, Trello",
+      icons: ["sql", "javascript", "html", "react"],
+  },
+  {
+    area: "Learning",
+    ref:"learning",
+    subskills:
+      "Redux, AWS, Nostr, more about data structures and algorithms with a focus on scripting, and dipping my toes into Java...",
+      icons: ["sql", "javascript", "html", "react"],
+  },
+];
