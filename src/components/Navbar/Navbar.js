b058e51eb04d0949404625e096d634a454386cc3
Victoria Lampard <vglampard@protonmail.com>
2023-04-04 18:20:18 +0100

    tried to shrink modal width

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "./lv.png";
-//[text-shadow:_0_07px_0_rgb(254_243_199_/_100%)] 
+//[text-shadow:_0_07px_0_rgb(254_243_199_/_100%)]
 
 export default function Navbar({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
@@ -17,17 +17,23 @@ export default function Navbar({ fixed }) {
                   src={logo}
                   alt="Victoria Lampard full stack developer"
                   className="h-[50px] drop-shadow-[0_5px_5px_rgba(254_243_199_/_100%)]"
-                 />
+                />
               </a>
               <a
                 className="text-lg  leading-4 inline-block mr-4 py-2  whitespace-nowrap uppercase text-slate-200 "
-                href="/" 
+                href="/"
               >
-                {" "}<p><span className = "drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-                Victoria Lampard </span><br></br>
-                <span className="text-base font-thin italic font-outline-2 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]" >Full Stack Dev</span></p>
+                {" "}
+                <p>
+                  <span className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                    Victoria Lampard{" "}
+                  </span>
+                  <br></br>
+                  <span className="text-base font-thin italic font-outline-2 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                    Full Stack Dev
+                  </span>
+                </p>
               </a>
-          
             </div>
 
             <button
@@ -49,30 +55,35 @@ export default function Navbar({ fixed }) {
             }
             id="example-navbar-danger"
           >
-            <ul className="flex text-base flex-col lg:flex-row list-none lg:ml-auto text-slate-200 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+            <ul className="flex text-base flex-col lg:flex-row list-none lg:ml-auto text-slate-200 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] uppercase">
               <li className="nav-item">
                 <Link
                   to="projects"
-                  className="px-3 py-2 pt-4 md:pt-2 lg:pt-2 flex items-center text-xs uppercase  leading-snug   "
+                  className="px-3 py-2 pt-4 md:pt-2 lg:pt-2 flex items-center uppercase  leading-snug   "
                 >
-                  <p className="hover:bg-yellow-100  hover:text-slate-800 px-1 ">Projects</p>
+                  <p className="hover:bg-yellow-100  hover:text-slate-800 px-1 ">
+                    Projects
+                  </p>
                 </Link>
               </li>
               <li className="nav-item">
                 <Link
                   to="skills"
-                  className="px-3 py-2 flex items-center text-xs uppercase  leading-snug "
+                  className="px-3 py-2 flex items-center   leading-snug "
                 >
-                     <p className="hover:bg-yellow-100 hover:text-slate-800  px-1">Skills</p>
+                  <p className="hover:bg-yellow-100 hover:text-slate-800  px-1">
+                    Skills
+                  </p>
                 </Link>
               </li>
-         
               <li className="nav-item">
                 <Link
                   to="contact"
-                  className="px-3 py-2 flex items-center text-xs uppercase  leading-snug "
+                  className="px-3 py-2 flex items-center   leading-snug "
                 >
-                   <p className="hover:bg-yellow-100 px-1 hover:text-slate-800 ">Contact</p>
+                  <p className="hover:bg-yellow-100 px-1 hover:text-slate-800 ">
+                    Contact
+                  </p>
                 </Link>
               </li>{" "}
             </ul>
