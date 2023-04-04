7c7f981284994dd372dd55b8e0fe1f4ac57c0081
Victoria Lampard <vglampard@protonmail.com>
2023-04-04 18:01:24 +0100

    contact page tweaked

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,43 @@
 import React from "react";
 import { ContactForm } from "./ContactForm/ContactForm";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // Component to render contact form and page title
 export default function Contact() {
+  // Function that triggers toast notification
+  const notify = () =>
+    toast("Email sent - I'll be in touch.", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      className: "text-base font-thin ",
+    });
+
   return (
-    <div className="w-[90%] text-3xl drop-shadow-[0_1px_1px_rgba(0,0,0,0.3)] pt-[30px]  md:w-[500px] flex flex-col items-center justify-center font-bold">
+    <div className="w-[90%] text-3xl drop-shadow-[0_1px_1px_rgba(0,0,0,0.3)] pt-[30px]  w-full flex flex-col items-center justify-center font-bold">
+      <ToastContainer
+        position="top-right"
+        autoClose={50000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        type="success"
+      />
+      <div className="md:w-[500px]">
       <h1 className="italic uppercase text-left w-full"> Get in touch... </h1>
-      <ContactForm />
+      <ContactForm notify={notify} />
+      </div>
     </div>
   );
 }
