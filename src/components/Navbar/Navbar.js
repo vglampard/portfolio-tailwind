45b7fc3f16f408aeee4ec6ec8bebf37b22909136
Victoria Lampard <vglampard@protonmail.com>
2023-04-04 18:05:33 +0100

    tried bg styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "./lv.png";
-//[text-shadow:_0_07px_0_rgb(254_243_199_/_100%)]
+//[text-shadow:_0_07px_0_rgb(254_243_199_/_100%)] 
 
 export default function Navbar({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
@@ -17,23 +17,17 @@ export default function Navbar({ fixed }) {
                   src={logo}
                   alt="Victoria Lampard full stack developer"
                   className="h-[50px] drop-shadow-[0_5px_5px_rgba(254_243_199_/_100%)]"
-                />
+                 />
               </a>
               <a
                 className="text-lg  leading-4 inline-block mr-4 py-2  whitespace-nowrap uppercase text-slate-200 "
-                href="/"
+                href="/" 
               >
-                {" "}
-                <p>
-                  <span className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-                    Victoria Lampard{" "}
-                  </span>
-                  <br></br>
-                  <span className="text-base font-thin italic font-outline-2 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-                    Full Stack Dev
-                  </span>
-                </p>
+                {" "}<p><span className = "drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                Victoria Lampard </span><br></br>
+                <span className="text-base font-thin italic font-outline-2 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]" >Full Stack Dev</span></p>
               </a>
+          
             </div>
 
             <button
@@ -61,9 +55,7 @@ export default function Navbar({ fixed }) {
                   to="projects"
                   className="px-3 py-2 pt-4 md:pt-2 lg:pt-2 flex items-center text-xs uppercase  leading-snug   "
                 >
-                  <p className="hover:bg-yellow-100  hover:text-slate-800 px-1 ">
-                    Projects
-                  </p>
+                  <p className="hover:bg-yellow-100  hover:text-slate-800 px-1 ">Projects</p>
                 </Link>
               </li>
               <li className="nav-item">
@@ -71,19 +63,16 @@ export default function Navbar({ fixed }) {
                   to="skills"
                   className="px-3 py-2 flex items-center text-xs uppercase  leading-snug "
                 >
-                  <p className="hover:bg-yellow-100 hover:text-slate-800  px-1">
-                    Skills
-                  </p>
+                     <p className="hover:bg-yellow-100 hover:text-slate-800  px-1">Skills</p>
                 </Link>
               </li>
+         
               <li className="nav-item">
                 <Link
                   to="contact"
                   className="px-3 py-2 flex items-center text-xs uppercase  leading-snug "
                 >
-                  <p className="hover:bg-yellow-100 px-1 hover:text-slate-800 ">
-                    Contact
-                  </p>
+                   <p className="hover:bg-yellow-100 px-1 hover:text-slate-800 ">Contact</p>
                 </Link>
               </li>{" "}
             </ul>
