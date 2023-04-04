172594ceff1c2073c44f1737c281cf518eb9b908
Victoria Lampard <vglampard@protonmail.com>
2023-04-04 13:24:58 +0100

    spacing fixed on modal padding

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -16,7 +16,7 @@ export default function Projects() {
         animate={{ opacity: 1}}
         transition={{ duration: 1 }}
       >
-    <div className="flex justify-center items-center h-[100%] pt-14">
+    <div className="flex justify-center items-center h-[100%] lg:pt-14 md:pt-14 pt-6">
       <Modal MODAL_STATES={MODAL_STATES} />
       
       <div className="w-[90%] lg:w-[70%] grid grid-cols-1 md:grid-cols-2 space-x-{10} gap-5 pb-20">
